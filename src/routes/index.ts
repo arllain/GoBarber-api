b5e31c1b71b165763e1c2389d1efda46645357e0
arllainm <arllain@gmail.com>
2020-04-13 21:35:37 -0300

    Importanto arquivos TS

---
+++ src/routes/index.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+
+const routes = Router();
+
+routes.get("/", (request, response) =>
+  response.json({ message: "Hello Typescript" })
+);
+export default routes;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import express from "express";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/", (request, response) =>
-  response.json({ message: "Hello Typescript" })
-);
+app.use(routes);
 
 app.listen(3333, () => {
   console.log("Server startes on port 3333!");
