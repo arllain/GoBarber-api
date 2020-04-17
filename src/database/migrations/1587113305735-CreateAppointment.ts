93f617c70e8e59caa3920c718cfe8556cd1fe136
arllainm <arllain@gmail.com>
2020-04-17 17:48:46 -0300

    Relacionamento nos models

--- src/database/migrations/1587113305735-CreateAppointment.ts
+++ src/database/migrations/1587113305735-CreateAppointment.ts
@@ -9,7 +9,7 @@ export default class CreateAppointment1587113305735
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
