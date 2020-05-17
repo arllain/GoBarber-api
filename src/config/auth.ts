8348c299293a673be8b04a3acd1fa88ceb61926a
arllainm <arllain@gmail.com>
2020-05-17 09:53:19 -0300

    Refatorando testes

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,7 +1,7 @@
 // Para gerar o md5 pode ser feito no site http://www.md5.cz/
 export default {
   jwt: {
-    secret: process.env.APP_SECRET,
+    secret: process.env.APP_SECRET || 'default',
     expiresIn: '1d',
   },
 };
