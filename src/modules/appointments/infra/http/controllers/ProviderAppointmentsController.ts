30ea9547b7f22d82e839919e98b3fd642e5a09c7
arllainm <arllain@gmail.com>
2020-05-25 22:35:44 -0300

    Serialização no cache

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 
 import { container } from 'tsyringe';
 import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
+import { classToClass } from 'class-transformer';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
@@ -14,10 +15,10 @@ export default class ProviderAppointmentsController {
 
     const appointments = await listProviderAppointments.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
